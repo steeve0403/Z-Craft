194f14e04bb6a8c493839e72e65a7e8888e8a6a5
steeve0403 <zychsteeve4@gmail.com>
2024-10-20 19:34:20 +0200

    Create structure for style scss

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
+      '@components': path.resolve(__dirname, './src/component'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+      '@utils': path.resolve(__dirname, './src/abstracts'),
     },
   },
   esbuild: {
