58c2bdd4c5af3b0040f1cb67dc481e464ac67a90
steeve0403 <zychsteeve4@gmail.com>
2024-10-20 19:02:35 +0200

    configure project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        addtionalData: `
+          @use "@/styles/variables"; 
+          @use "@/styles/mixins";
+        `
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
