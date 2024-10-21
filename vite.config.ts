9a5938a10b096a4c15ab649aa86939b58882e70b
steeve0403 <zychsteeve4@gmail.com>
2024-10-21 15:27:52 +0200

    start structure sass and adding variables, mixins && utilities for first step on styling app

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,39 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "@/styles/abstracts/variables" as *; 
-          @use "@/styles/abstracts/mixins" as *;
-        `
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
         },
-      },
     },
-  },
+    esbuild: {
+        jsxInject: `import React from 'react'`,
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+          @use "@/styles/abstracts/index" as *;
+        `,
+            },
+        },
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
 })
