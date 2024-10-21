64f26564b21629a296d010e18ae258c5c414369a
steeve0403 <zychsteeve4@gmail.com>
2024-10-21 13:22:48 +0200

    prepare homepage and add on router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
-import {ThemeProvider} from "./contexts/ThemeContext.tsx";
-import ErrorBoundary from "./components/ErrorBoundary.tsx";
+import React, { Suspense } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {ThemeProvider} from './contexts/ThemeContext.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
+import {routes} from './routes.ts';
+import LoadingSpinner from "./components/LoadingSpinner.tsx";
 
 const App: React.FC = () => {
     return (
@@ -9,7 +11,13 @@ const App: React.FC = () => {
             <ThemeProvider>
                 <Router>
                     <div className="layout">
-
+                        <Suspense fallback={<LoadingSpinner />}>
+                            <Routes>
+                                {routes.map((route) => (
+                                    <Route key={route.path} path={route.path} element={<route.component />} />
+                                ))}
+                            </Routes>
+                        </Suspense>
                     </div>
                 </Router>
             </ThemeProvider>
