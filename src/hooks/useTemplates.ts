3229b84b2b4a86c636d19f1ab806ed250f0c603f
steeve0403 <zychsteeve4@gmail.com>
2024-10-27 10:14:43 +0100

    reset useTemplates.ts for moment

--- src/hooks/useTemplates.ts
+++
@@ -1,7 +0,0 @@
-import {useLiveQuery} from "dexie-react-hooks";
-import {db} from "../db/dexieConfig.ts";
-
-
-export const useTemplates = () => useLiveQuery(() => db.templates.toArray(), []);
-
-export const useTemplate = (templateID: string) => useLiveQuery(() => db.templates.get(templateID), [templateID]);
